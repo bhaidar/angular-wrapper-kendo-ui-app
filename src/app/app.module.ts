f4b77e63169c4f59d209af648b8350b8e1cf4e81
Bilal Haidar <bhaidar@gmail.com>
2018-08-16 12:32:15 +0300

    Connected the library to the app and added some npm utility scripts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularWrapperKendoUiModule } from 'angular-wrapper-kendo-ui'; 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularWrapperKendoUiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
